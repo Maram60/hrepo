84ba2a313280e40ebe7d196a0f38647426596062
Maram60 <138799375+Maram60@users.noreply.github.com>
2024-03-28 22:49:50 +0000

    create layout and basic templates

---
+++ green_garden_web/plants_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+#import clubs views
+from plants_app import views
+
+
+
+urlpatterns = [    
+    path('', views.index, name='index'),
+    path('plants', views.plants),
+    path('plants/<int:pId>', views.plant)
+] 
--- green_garden_web/plants_app/views.py
+++ green_garden_web/plants_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    # This view returns the index page for Green Garden Web App
+    return render(request, 'plantsmodule/index.html')
+
+def plants(request):
+    # This view returns the plants list page for Green Garden Web App
+    return render(request, 'plantsmodule/plantList.html')
+
+def plant(request, plant_id):
+    # This view returns the details of a specific plant for Green Garden Web App
+
+    # Example plant data
+    plant1 = {'id': 1, 'name': 'Aloe Vera', 'type': 'Succulent', 'watering_frequency': 'Weekly', 'sunlight_requirement': 'Partial'}
+    plant2 = {'id': 2, 'name': 'Snake Plant', 'type': 'Perennial', 'watering_frequency': 'Bi-weekly', 'sunlight_requirement': 'Low to Moderate'}
+
+    targetPlant = None
+    if plant1['id'] == plant_id: targetPlant = plant1
+    if plant2['id'] == plant_id: targetPlant = plant2
+
+    if targetPlant == None: return redirect('/plants')
+
+    context = {'plant': targetPlant}  # 'plant' is the variable name accessible by the template
+    return render(request, 'plantsmodule/plant.html', context)
