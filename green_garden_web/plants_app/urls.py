e2446c515f078cc73ced30a85d5dbb438097f137
Maram60 <138799375+Maram60@users.noreply.github.com>
2024-04-25 21:34:32 +0000

    lecture 8,9

--- green_garden_web/plants_app/urls.py
+++ green_garden_web/plants_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('plants', views.plants),
     path('plants/<int:pId>', views.plant),
+      path('plants/add', views.add_plant, name='add_plant'),
+    path('plants/edit/<int:pId>', views.edit_plant, name='edit_plant'),
     path('plantsSearch', views.filter_plant, name="filter_plant")
 ] 
--- green_garden_web/plants_app/views.py
+++ green_garden_web/plants_app/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from .models import Plant  
+from django.db.models import Count, Avg
+from django.shortcuts import redirect, render
+from .models import Plant
+from django.views.decorators.csrf import csrf_exempt
+
 
 def plants(request):
-    plants = Plant.objects.all() 
-    return render(request, 'plantsmodule/plantsList.html', {'plants': plants})
+    plants = Plant.objects.all()
+    
+    # Aggregations
+    plant_count = Plant.objects.count()
+    type_distribution = Plant.objects.values('type').annotate(total=Count('type'))
+    average_watering_frequency = Plant.objects.annotate(num_watering=Count('watering_frequency')).order_by('-num_watering')[:5]  # Top 5 watering frequencies
+
+    context = {
+        'plants': plants,
+        'plant_count': plant_count,
+        'type_distribution': type_distribution,
+        'average_watering_frequency': average_watering_frequency
+    }
+    return render(request, 'plantsmodule/plantsList.html', context)
 
 def index(request):
     return render(request, 'plantsmodule/index.html')
@@ -27,3 +42,29 @@ def filter_plant(request):
         plants = plants.filter(watering_frequency__icontains=watering_frequency)
 
     return render(request, 'plantsmodule/plantsSearch.html', {'plants': plants})
+
+
+@csrf_exempt
+def add_plant(request):
+    if request.method == 'POST':
+        Plant.objects.create(
+            name=request.POST['name'],
+            type=request.POST['type'],
+            watering_frequency=request.POST['watering_frequency'],
+            sunlight_requirement=request.POST['sunlight_requirement']
+        )
+
+    return render(request, 'plantsmodule/add_plant.html')
+
+@csrf_exempt
+def edit_plant(request, pId):
+    plant = Plant.objects.get(id=pId)
+    if request.method == 'POST':
+        plant.name = request.POST['name']
+        plant.type = request.POST['type']
+        plant.watering_frequency = request.POST['watering_frequency']
+        plant.sunlight_requirement = request.POST['sunlight_requirement']
+        plant.save()
+
+    return render(request, 'plantsmodule/edit_plant.html', {'plant': plant})
+
